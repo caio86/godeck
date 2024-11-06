2160b40e2cc874ff9b0e970b88596654b58eb7e9
caio86 <caioluiz86@gmail.com>
2024-11-06 00:20:04 -0300

    feat(flake): add `godeck` package configuration
    - Configured `godeck` Go module as a package in `flake.nix`.
    - Added `buildGoModule` with relevant attributes to streamline building and managing Go modules within Nix.
    - Targeted `cmd/godeck` as the primary sub-package for this configuration.

--- flake.nix
+++ flake.nix
@@ -14,6 +14,24 @@
         function: nixpkgs.lib.genAttrs systems (system: function (nixpkgs.legacyPackages.${system}));
     in
     {
+      packages = forAllSystems (
+        pkgs:
+        let
+          inherit (pkgs) buildGoModule;
+        in
+        {
+          godeck = buildGoModule {
+            pname = "godeck";
+            version = "1.0.0";
+
+            src = ./.;
+
+            vendorHash = null;
+
+            subPackages = [ "cmd/godeck" ];
+          };
+        }
+      );
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           packages = with pkgs; [
